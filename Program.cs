b728d7a7caec39d13d5d74c3e9b79c3c38a38ffd
ThunderbirdOne <vincent.vanbeeumen@gmail.com>
2017-04-15 11:11:43 +0200

    Initial push

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace ConcatCSFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFolder = ConfigurationManager.AppSettings["input"];
+            string output = ConfigurationManager.AppSettings["output"];
+            string[] excludeList = ConfigurationManager.AppSettings["exclude"].Split(',');
+
+            String[] allfiles = Directory.GetFiles(inputFolder, "*.cs", SearchOption.AllDirectories);
+
+            File.WriteAllText(output, string.Empty); //clear file
+            foreach (var file in allfiles)
+            {
+                FileInfo info = new FileInfo(file);
+                if (excludeList.Contains(info.Name) || info.Name.StartsWith("TemporaryGeneratedFile")) continue;
+
+                string[] readText = File.ReadAllLines(info.FullName);
+                if(info.Name != "Player.cs") //if not the main class, remove all the usings
+                {
+                    readText = readText.Where(r => !r.StartsWith("using")).ToArray();
+                }
+
+                File.AppendAllLines(output, readText);
+            }
+        }
+    }
+}
